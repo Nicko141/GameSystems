0e945c3006490272fc8f9a91c6cbbffbd7ffe2bc
Nicko141 <60990418+Nicko141@users.noreply.github.com>
2020-06-29 11:27:19 +1000

    fix key binds. inv, chest and shop panels

--- Assets/Scripts/KeyBindManager.cs
+++ Assets/Scripts/KeyBindManager.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class KeyBindManager : MonoBehaviour
 {
     public static Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
-    public Text forward, backward, left, right, jump, crouch, sprint, interact, inventory;
+
+    [Serializable]
+    public struct KeyUISetup //creating a custom variable type of group variables
+    {
+        public string keyName;
+        public Text keyDisplayText;
+        public string defaultKey;
+    }
+    public KeyUISetup[] baseSetup;
+
+
     public GameObject currentKey;
     public Color32 changed = new Color32(39, 171, 249, 255);
     public Color32 selected = new Color32(239, 116, 36, 255);
@@ -14,26 +25,16 @@ public class KeyBindManager : MonoBehaviour
 
     void Start()
     {
-        keys.Add("Forward", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Forward","W")));
-        keys.Add("Backward", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Backward", "S")));
-        keys.Add("Left", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A")));
-        keys.Add("Right", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D")));
-
-        keys.Add("Jump", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Jump", "Space")));
-        keys.Add("Crouch", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Crouch", "LeftControl")));
-        keys.Add("Sprint", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Sprint", "LeftShift")));
-        keys.Add("Interact", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Interact", "E")));
-        keys.Add("Inventory", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Inventory", "Tab")));
-
-        forward.text = keys["Forward"].ToString();
-        backward.text = keys["Backward"].ToString();
-        left.text = keys["Left"].ToString();
-        right.text = keys["Right"].ToString();
-        jump.text = keys["Jump"].ToString();
-        crouch.text = keys["Crouch"].ToString();
-        sprint.text = keys["Sprint"].ToString();
-        interact.text = keys["Interact"].ToString();
-        inventory.text = keys["Inventory"].ToString();
+        if (baseSetup.Length == 0)//check if dictionary already has keys saved
+        {//for loop to add keys to the dictionary
+            for (int i = 0; i < baseSetup.Length; i++)
+            {
+                //add key according to the save or default
+                keys.Add(baseSetup[i].keyName, (KeyCode)Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(baseSetup[i].keyName, baseSetup[i].defaultKey)));
+                //sets display text for keys
+                baseSetup[i].keyDisplayText.text = keys[baseSetup[i].keyName].ToString();
+            }
+        }
     }
     private void OnGUI()
     {
